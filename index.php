6d4ce8ef786465ebabf08ba203c3fd5c499d1eb0
kotos <tss-05@ya.ru>
2017-08-14 03:00:24 +0400

    im spb

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
+/* ****************************************************************************
+ *
+ *
+ * local dir: http://labs-profit-php-1/
+ * */
+echo 'My first string! =)';
+echo '<br>';
 echo 'My first string! =)';
 // ear
 // New line for Git Hub
 // Курс: [pr-of-it.ru] PHP-1: ВВЕДЕНИЕ В ПРОФЕССИЮ
-//labs-profit-php-1
+// labs-profit-php-1
 // im 101
+// im spb
+
 
 
 ?>
